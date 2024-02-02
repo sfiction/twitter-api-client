c4be0ed1cb6d65eaf65dff8657abd7ab442f1b41
sfiction <sfiction.r@gmail.com>
2024-02-02 10:33:08 +0800

    feat: wrap get tweets

--- twitter/api.py
+++ twitter/api.py
@@ -2,8 +2,10 @@
 import json
 import sys
 from itertools import groupby
+from typing import *
 
 from .scraper import Scraper
+from .transform import build_timeline_entry, build_tweet
 
 def print_json(obj, **kwargs):
     kwargs = {'indent': 2, 'ensure_ascii': False, 'default': str} | kwargs
@@ -21,146 +23,6 @@ def Get(obj, keys, strict=False):
                 return None
     return obj
 
-def unwrap_tweet(obj):
-    obj = obj['itemContent']
-    # TODO: clientEventInfo
-    assert obj['itemType'] == 'TimelineTweet', obj['itemType']
-    assert obj['__typename'] == 'TimelineTweet', obj['__typename']
-    assert obj['tweetDisplayType'] in ['Tweet', 'MediaGrid'], obj['tweetDisplayType']
-    obj = obj['tweet_results']
-    if not 'result' in obj: return None
-
-    def build_user(obj):
-        obj = obj['user_results']
-        obj = obj['result']
-        if obj['__typename'] == 'User':
-            obj.pop('__typename')
-        else:
-            assert False, f'unknown result.__typename {obj["__typename"]}'
-        id_str = obj['rest_id']
-        obj = obj['legacy']
-
-        t = {}
-        t['id'] = int(id_str)
-        t['id_str'] = id_str
-        SELECT_KEYS = [
-            'created_at',
-            'description',
-            'favourites_count',
-            'followers_count',
-            'friends_count',
-            'listed_count',
-            'media_count',
-            'name',
-            'screen_name',
-            'statuses_count',
-        ]
-        for key in SELECT_KEYS:
-            #if key in obj:
-            t[key] = obj.pop(key)
-        return t
-
-    def build_tweet(obj):
-        obj = obj['result']
-        if obj['__typename'] == 'TweetWithVisibilityResults':
-            obj = obj['tweet']
-        elif obj['__typename'] == 'Tweet':
-            obj.pop('__typename')
-        else:
-            assert False, f'unknown result.__typename {obj["__typename"]}'
-        id_str = obj['rest_id']
-        user = build_user(obj.pop('core'))
-        obj = obj['legacy']
-        obj.pop('extended_entities', None)
-
-        t = {}
-        t['id'] = int(id_str)
-        t['id_str'] = id_str
-        t['user'] = user
-        if 'retweeted_status_result' in obj:
-            t['retweeted_status'] = build_tweet(obj.pop('retweeted_status_result'))
-        if 'quoted_status_result' in obj:
-            t['quoted_status'] = build_tweet(obj.pop('quoted_status_result'))
-        if 'entities' in obj:
-            entities = obj.pop('entities')
-            if 'media' in entities:
-                t['media'] = entities.pop('media')
-                for media in t['media']:
-                    media.pop('features', None)
-                    media.pop('sizes', None)
-                    media.pop('indices', None)
-                    media['original_info'].pop('focus_rects', None)
-                    media['id'] = int(media['id_str'])
-            if 'hashtags' in entities:
-                t['hashtags'] = [tag['text'] for tag in entities['hashtags']]
-            if 'urls' in entities:
-                t['urls'] = [{key: url[key] for key in ['expanded_url', 'url']} for url in entities['urls']]
-        t['text'] = obj.pop('full_text')
-
-        SELECT_KEYS = [
-            'bookmark_count',
-            # 'bookmarked',
-            'created_at',
-            'favorite_count',
-            # 'favorited',
-            'lang',
-            'possibly_sensitive',
-            'quote_count',
-            'reply_count',
-            'retweet_count',
-            # 'retweeted',
-        ]
-        for key in SELECT_KEYS:
-            if key in obj:
-                t[key] = obj.pop(key)
-        assert t['id_str'] == obj.pop('id_str')
-        assert t['user']['id_str'] == obj.pop('user_id_str')
-        return t
-
-    obj = build_tweet(obj)
-    return obj
-
-def unwrap_timeline_item(obj):
-    entry_id = obj["entryId"]
-    if entry_id.startswith('tweet-'):
-        obj = obj['content']
-        assert obj['entryType'] == 'TimelineTimelineItem', obj['entryType']
-        assert obj['__typename'] == 'TimelineTimelineItem', obj['__typename']
-        ret = [unwrap_tweet(obj)]
-    elif entry_id.startswith('profile-conversation-'):
-        obj = obj['content']
-        assert obj['entryType'] == 'TimelineTimelineModule', obj['entryType']
-        assert obj['__typename'] == 'TimelineTimelineModule', obj['__typename']
-        items = obj['items']
-        ret = []
-        for obj in items:
-            assert obj['entryId'].startswith('profile-conversation')
-            obj = obj['item']
-            ret.append(unwrap_tweet(obj))
-    elif entry_id.startswith('profile-grid-'):
-        obj = obj['content']
-        assert obj['entryType'] == 'TimelineTimelineModule', obj['entryType']
-        assert obj['__typename'] == 'TimelineTimelineModule', obj['__typename']
-        items = obj['items']
-        ret = []
-        for obj in items:
-            assert obj['entryId'].startswith('profile-grid-')
-            obj = obj['item']
-            ret.append(unwrap_tweet(obj))
-    else:
-        PREFIXES = [
-            'promoted-tweet-',
-            'who-to-follow-',
-            'cursor-bottom-',
-            'cursor-top-',
-        ]
-        if all(not entry_id.startswith(prefix) for prefix in PREFIXES):
-            print(f'unknown entryId {entry_id}', file=sys.stderr)
-            raise Exception(f'unknown entryId {entry_id}')
-        ret = []
-    ret = [obj for obj in ret if not obj is None]
-    return ret
-
 def get_cursor(entries):
     for entry in entries:
         entry_id = entry.get('entryId', '')
@@ -173,7 +35,7 @@ def get_cursor(entries):
 def get_id(obj):
     return obj['id']
 
-class Api(object):
+class Api:
     def __init__(self, req, debug=False):
         for line in req.split('\n'):
             if line.startswith('Cookie: '):
@@ -194,8 +56,8 @@ class Api(object):
         cursor = get_cursor(entries)
         entries = [entry for entry in entries]
         if pin_entry:
-            pin_entry = unwrap_timeline_item(pin_entry)[0]
-        entries = sum(map(unwrap_timeline_item, entries), [])
+            pin_entry = build_timeline_entry(pin_entry)[0]
+        entries = sum(map(build_timeline_entry, entries), [])
         return entries, pin_entry, cursor
 
     def _get_user_timeline(self, method, user_id, *, since=None, until=None, count=None):
@@ -218,6 +80,12 @@ class Api(object):
         tweets = sorted((tweet for tweet in tweets if since < tweet['id'] <= until),
             key=get_id, reverse=True)
         tweets = [list(gp)[0] for _, gp in groupby(tweets, key=get_id)]
+        return tweets
+
+    def get_tweets(self, tweet_ids: list[Union[str, int]]):
+        tweet_ids = list(map(int, tweet_ids))
+        tweets = self.scraper.tweets_by_id(tweet_ids)
+        tweets = [build_tweet(tweet[0]['data']['tweetResult']) for tweet in tweets]
         return tweets
 
     def get_user_tweets(self, *args, **kwargs):
